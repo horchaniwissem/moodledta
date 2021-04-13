7c7a2ccaadcf14afbb4a9b990b56219964aa8641
Horchani Wissem <horchani.wissem@icloud.com>
2021-04-13 15:23:11 +0100

    moodiledata

---
+++ lang/fr/block_studentstracker.php
@@ -0,0 +1,53 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_studentstracker', language 'fr', version '3.10'.
+ *
+ * @package     block_studentstracker
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['blocktitle'] = 'Titre du bloc';
+$string['color_days'] = 'Couleur seuil 1';
+$string['color_days_critical'] = 'Couleur seuil critique';
+$string['color_never'] = 'Couleur aucun accès';
+$string['days'] = 'Nombre de jours pour seuil 1';
+$string['days_critical'] = 'Nombre de jours pour seuil critique';
+$string['footer'] = 'Texte pied de page';
+$string['groups'] = 'Groupes à suivre';
+$string['header'] = 'Texte à côté du nombre d\'utilisateurs';
+$string['header_fine'] = 'Texte si tout est ok';
+$string['nogroups'] = 'Aucun (tous les utilisateurs)';
+$string['pluginname'] = 'Suivi d\'étudiants';
+$string['role'] = 'Suivre tous les rôles ?';
+$string['roles'] = 'Rôles autorisés à voir le bloc';
+$string['roletrack'] = 'Rôles à suivre';
+$string['roleview'] = 'Rôles autorisés à voir le bloc';
+$string['studentstracker:addinstance'] = 'Ajouter un nouveau bloc Suivi étudiants';
+$string['studentstracker:editadvance'] = 'Modification avancée';
+$string['studentstracker:view'] = 'Afficher le résultat du suivi des étudits';
+$string['text_footer'] = 'Message d\'incitation à la prise de contact';
+$string['text_footer_content'] = 'Contactez-les !';
+$string['text_header'] = 'Utilisateurs absents';
+$string['text_header_fine'] = 'Tout va bien !';
+$string['text_never'] = 'Texte si l\'utilisateur n\'a jamais accédé au cours';
+$string['text_never_content'] = 'aucun accès';
+$string['truncate'] = 'Afficher seulement n résultats';
